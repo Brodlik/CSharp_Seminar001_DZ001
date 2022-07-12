9a4a5b8a4cca6eafecfa2966d498cfda722fc645
Svetlana <brodlik@me.com>
2022-07-12 16:00:34 +0300

    Zadacha002_MaxMin

---
+++ Zadacha002_Max_Min/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{
+    Console.Write ("max= ");
+    Console.WriteLine(A);
+    Console.Write ("min= ");
+    Console.WriteLine(B);
+}
+else
+{
+    Console.Write ("max= ");
+    Console.WriteLine(B);
+    Console.Write ("min= ");
+    Console.WriteLine(A);
+}
